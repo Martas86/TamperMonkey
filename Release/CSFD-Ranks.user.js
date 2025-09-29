bc69b3a9e3b04ac01276c4e79f6bc5505109500b
Martin <m.stibal@gmail.com>
2025-09-29 19:02:43 +0200

    HTML change

--- Release/CSFD-Ranks.user.js
+++ Release/CSFD-Ranks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSFD - Ranks
 // @namespace    https://github.com/Martas86/TamperMonkey
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.csfd.cz/zebricky/vlastni-vyber/*
@@ -16,27 +16,27 @@
     // Your code here...
 	$ = window.jQuery;
     function rwsCntRecalc() {
-        $("#xRwsCnt").text("(" + $("#results table.content tbody tr td.order:visible").length + ")");
+        $("#xRwsCnt").text("(" + $(".box-content article:visible").length + ")");
     }
 
     function dofiltruj() {
         var minRatCnt = parseInt($("#xMinRatingCnt").val());
         var notRateByMe = $('#xNotRateByMe').is(':checked');
-        $("#results table.content tbody tr").each(function() {
-            $(this).toggle((parseInt($("td.count", this).text(), 10) >= minRatCnt) && (!notRateByMe || $("td.rating", this).html() == ""));
+        $(".box-content article").each(function() {
+            $(this).toggle((parseInt($("div.rating-total", this).text().replace(/\D/g, ""), 10) >= minRatCnt) && (!notRateByMe || $("div.rating-mine", this).length == 0));
         });
         rwsCntRecalc();
     }
 
     function dofiltrujZrus() {
-        $("#results table.content tbody tr").show();
+        $(".box-content article").show();
         rwsCntRecalc();
     }
 
     $(document).ready(function() {
-        $(".search-item-full").append('<div style="width: 100%;">\
-<span>Min. poč. hodnocení:<span><input id="xMinRatingCnt" style="width: 50px;" type="input" value="500" />\
-<span style="margin: 0px 0px 0px 10px;">Mnou nehodnoceno:<span><input id="xNotRateByMe" style="" type="checkbox" />\
+        $(".search-item-full").first().append('<div style="width: 100%;">\
+<span>Min. poč. hodnocení:</span><input id="xMinRatingCnt" style="width:50px;min-width:50px;" type="input" value="500" />\
+<span style="margin: 0px 0px 0px 10px;">Mnou nehodnoceno:</span><input id="xNotRateByMe" style="width:13px;height:13px;min-width:13px;" type="checkbox" />\
 <button id="xDofiltruj" style="margin: 0px 0px 0px 10px;" type=button>Dofiltruj</button><button id="xZrus" style="margin: 0px 0px 0px 10px;" type=button>Zruš</button>\
 <span id="xRwsCnt" style="float: right;">()</span>\
 </div>');
