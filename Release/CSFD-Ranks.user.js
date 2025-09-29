29a4081da1ff4a7143b2511167cf18cf6297ef14
Martin <m.stibal@gmail.com>
2025-09-29 18:35:22 +0200

    Test

--- Release/CSFD-Ranks.user.js
+++ Release/CSFD-Ranks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSFD - Ranks
 // @namespace    https://github.com/Martas86/TamperMonkey
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.csfd.cz/zebricky/vlastni-vyber/*
