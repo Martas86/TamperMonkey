8b144b7bd7ed25f3080e7fa9ae603e9157759ba6
Martin <m.stibal@gmail.com>
2025-09-29 18:18:04 +0200

    Url change

--- Release/CSFD-Ranks.user.js
+++ Release/CSFD-Ranks.user.js
@@ -4,7 +4,7 @@
 // @version      0.3
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.csfd.cz/zebricky/specificky-vyber/*
+// @match        https://www.csfd.cz/zebricky/vlastni-vyber/*
 // @grant        none
 // @downloadURL  https://github.com/Martas86/TamperMonkey/raw/master/Release/CSFD-Ranks.user.js
 // @updateURL    https://github.com/Martas86/TamperMonkey/raw/master/Release/CSFD-Ranks.user.js
